5220652bd7c5f3b4467bc952e05ef000916af388
Megrez <seywq@outlook.com>
2024-09-09 17:06:27 +0800

    third commit

--- src/main/java/com/megrez/Main.java
+++ src/main/java/com/megrez/Main.java
@@ -1,8 +1,14 @@
 package com.megrez;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("..");
         System.out.println("...");
         System.out.println("....");
+        Scanner sc= new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i);
     }
 }
