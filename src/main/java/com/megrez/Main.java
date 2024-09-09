ef2952bca0ce0672c17606a90062c45baa1aedf2
Megrez <seywq@outlook.com>
2024-09-09 18:03:51 +0800

    push commit

--- src/main/java/com/megrez/Main.java
+++ src/main/java/com/megrez/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("...");
         System.out.println("....");
         System.out.println(".!!..");
+        System.out.println("Push test!");
         System.out.println(".??.");
         Scanner sc= new Scanner(System.in);
         int i = sc.nextInt();
